7138a2a0c8189169d9bf110bb18971c6ec6f67d9
RichardJTelford <Richard.Telford@uib.no>
2025-12-15 01:10:34 +0100

    update yaml & make books

--- make_books.R
+++ make_books.R
@@ -1,4 +1,16 @@
 
+# copy assets
+fs::dir_create("docs/figures")
+fs::file_copy("figures/icons_all.png", "docs/figures/icons_all.png")
+fs::file_copy("figures/favicon.png", "docs/figures/favicon.png")
+
 # Front page
-quarto::quarto_render("frontpage")
+quarto::quarto_render()
+
+
+# debug version
+withr::with_envvar(new = c("QUARTO_PROFILE" = "debug"), {
+  quarto::quarto_preview(".")  # Create basic.html
+})
+
 
